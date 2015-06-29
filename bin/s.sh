14fbef005ee9c7356b3626331f611f3c99d08bbc
jamestan <jamestanms@hotmail.com>
2015-06-29 10:21:53 -0400

    Reverted s.sh to the JDK 1.7_u21

--- bin/s.sh
+++ bin/s.sh
@@ -1,6 +1,7 @@
 #change JAVA_HOME accordingly
 #export JAVA_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_45.jdk/Contents/Home
+#export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_45.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_21.jdk/Contents/Home export PATH=$JAVA_HOME/bin:$PATH
 
-#java -javaagent:/service/appengine-java-sdk-1.4.2/lib/agent/appengine-agent.jar -classpath /service/appengine-java-sdk-1.4.2/lib/appengine-tools-api.jar com.google.appengine.tools.development.DevAppServerMain --port=8080 --address=0.0.0.0 /service/gaej/GoogleAppEngine/war
-cp ../lib/standalone/selenium-server-standalone-2.35.0.jar ../war/WEB-INF/lib
+
+java -javaagent:/appengine-java-sdk/lib/agent/appengine-agent.jar -classpath /appengine-java-sdk/lib/appengine-tools-api.jar com.google.appengine.tools.development.DevAppServerMain --port=8888 --address=0.0.0.0 ../war
