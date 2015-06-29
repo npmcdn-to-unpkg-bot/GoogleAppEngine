1bb7e6803d8198da10da91db83d11af50019d480
TANJ3 <you@example.com>
2015-06-28 22:47:05 -0400

    Down JDK 7 to u20 due to http://bugs.java.com/bugdatabase/view_bug.do?bug_id=8021205

--- bin/ss.sh
+++ bin/ss.sh
@@ -1,5 +1,6 @@
 #change JAVA_HOME accordingly
 #export JAVA_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_45.jdk/Contents/Home
+#export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_45.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_21.jdk/Contents/Home export PATH=$JAVA_HOME/bin:$PATH
 
-java -javaagent:/appengine-java-sdk/lib/agent/appengine-agent.jar -classpath /appengine-java-sdk/lib/appengine-tools-api.jar com.google.appengine.tools.development.DevAppServerMain --port=8888 --address=0.0.0.0 "$g"/../war
+java -javaagent:/appengine-java-sdk/lib/agent/appengine-agent.jar -classpath /appengine-java-sdk/lib/appengine-tools-api.jar com.google.appengine.tools.development.DevAppServerMain --port=8888 --address=0.0.0.0 ../war
