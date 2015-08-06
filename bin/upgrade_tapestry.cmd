2969728b91800a797f82531929c25a8fcc7c3700
wf <white.flame72@gmail.com>
2015-08-05 20:36:27 -0400

    Upgraded Tapestry to 5.3.8

--- bin/upgrade_tapestry.cmd
+++ bin/upgrade_tapestry.cmd
@@ -1,10 +1,9 @@
-set JAVA_HOME=C:\jdk1.6.0_45
-REM Uncomment the above mvn lines and change the version accordingly
-set T_VERSION=5.3.7
-REM goto http://tapestry.apache.org/download.html, download all jar files and copy them into 1/
-REM remove 1/*.* when you are done!
+set T_VERSION=5.3.8
 
-cd 1
+REM goto http://tapestry.apache.org/download.html, download all jar files and copy them into 1/
+
+cd %USERPROFILE%\Downloads\apache-tapestry-5.3.8-bin
+
 copy tapestry-core-%T_VERSION%.jar c:\GoogleAppEngine\war\WEB-INF\lib
 copy tapestry-func-%T_VERSION%.jar c:\GoogleAppEngine\war\WEB-INF\lib
 copy tapestry-ioc-%T_VERSION%.jar c:\GoogleAppEngine\war\WEB-INF\lib
