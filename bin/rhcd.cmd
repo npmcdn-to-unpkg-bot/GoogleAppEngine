e384ee615874be7f786051e7ecff97898b3bcc47
wf <white.flame72@gmail.com>
2015-08-16 15:00:43 -0400

    CapeDwarf changes

--- bin/rhcd.cmd
+++ bin/rhcd.cmd
@@ -6,16 +6,20 @@ set CAPEDWARF=C:\gae
 pause
 
 :### Local CapeDwarf Only ###
-del /f/s/q %LOCAL_CAPEDWARF%\deployments\ROOT.war > nul
-copy /S/Y ..\.openshift\config\standalone.xml %LOCAL_CAPEDWARF%\standalone\configuration\
+del /f/s/q %LOCAL_CAPEDWARF%\standalone\deployments\ROOT.war > nul
+:pause
+xcopy /S/Y ..\.openshift\config\standalone.xml %LOCAL_CAPEDWARF%\standalone\configuration\
 xcopy /S/Y ..\war %LOCAL_CAPEDWARF%\standalone\deployments\ROOT.war\
-copy rcd %LOCAL_CAPEDWARF%\bin
+copy rcd.cmd %LOCAL_CAPEDWARF%\bin
+:pause
 
 :#### OpenShift ###
-del /f/s/q %CAPEDWARF%\deployments\ROOT.war > nul
+del /f/s/q %CAPEDWARF%\deployments\ROOT.war
+pause
 xcopy /S/Y ..\war %CAPEDWARF%\deployments\ROOT.war\
 copy skip_maven_build %CAPEDWARF%\.openshift\markers
-copy rhcp %CAPEDWARF%
+copy rhcp.cmd %CAPEDWARF%
+pause
 
 :cd %CAPEDWARF%
 
