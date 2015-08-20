039cec732817dff381bb8e7b3e50731541316d98
wf <white.flame72@gmail.com>
2015-08-19 21:48:01 -0400

    2Share: fix NPE on local DEV server

--- bin/setEnv.cmd
+++ bin/setEnv.cmd
@@ -3,7 +3,7 @@
 set PROJECT_HOME=..
 
 set JAVA_HOME=C:\jdk1.7.0_51
-set GAE_JAVA_SDK_HOME=C:\appengine-java-sdk-1.9.12
+set GAE_JAVA_SDK_HOME=C:\appengine-java-sdk-1.9.25
 set MAVEN_HOME=C:\apache-maven-2.2.1
 set ROO_HOME=C:\spring-roo-1.1.0.M3
 set ANT_HOME=C:\apache-ant-1.6.5
