3ce78baa2d27f66c722a388245ee39d1fc9d4f6e
wf <wf@10.174.152.255>
2015-12-18 11:55:53 -0500

    Fix deploy command on Mac

--- bin/deploy_mac.sh
+++ bin/deploy_mac.sh
@@ -1,8 +1,5 @@
 #! /usr/bin/env bash
 
-#WORKAROUND for SDK 1.8.4 issue with selenium server 2.35.0!!!!
-#rm ../war/WEB-INF/lib/selenium-server-standalone-2.35.0.jar
-
 #Need to create soft link to your GAEJ home e.g.
 #ln -s /Applications/appengine-java-sdk-1.8.7 /appengine-java-sdk
 #sudo ln -s /Applications/appengine-java-sdk-1.8.8 /appengine-java-sdk
@@ -12,7 +9,7 @@
 #Also need to make sure Java is installed properly, generally available at /usr/bin/java and 
 #change JAVA_HOME accordingly
 #export JAVA_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_45.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_80.jdk/Contents/Home
 #export JAVA_HOME='/System/Library/Frameworks/JavaVM.framework/Home'
 export GAE_JAVA_SDK_HOME='/appengine-java-sdk'
 #export PATH=${PATH}:$JAVA_HOME/bin:$GAE_JAVA_SDK_HOME/bin
@@ -20,5 +17,5 @@ PROJECT_HOME='/project_home'
 export JAVA_OPTS='-XX:MaxPermSize=1024m'
 #Reference: https://developers.google.com/appengine/docs/java/tools/uploadinganapp
 #$GAE_JAVA_SDK_HOME/bin/appcfg.sh ${JAVA_OPTS} --enable_jar_splitting update ${PROJECT_HOME}/GoogleAppEngine/war
+echo $GAE_JAVA_SDK_HOME/bin/appcfg.sh --use_java7 --enable_jar_splitting update ${PROJECT_HOME}/war
 $GAE_JAVA_SDK_HOME/bin/appcfg.sh --use_java7 --enable_jar_splitting update ${PROJECT_HOME}/war
-#$GAE_JAVA_SDK_HOME/bin/appcfg.sh ${JAVA_OPTS} --no_cookies --use_java7 --enable_jar_splitting update ${PROJECT_HOME}/GoogleAppEngine/war
