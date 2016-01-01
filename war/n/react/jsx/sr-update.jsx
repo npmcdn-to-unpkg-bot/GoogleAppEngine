9553733bbcdd31096a350e173e0d282f745abf39
wf <wf@10.174.152.255>
2016-01-01 17:33:37 -0500

    SR2: mostly complete reactjs minus URL type validation

--- war/n/react/jsx/sr-update.jsx
+++ war/n/react/jsx/sr-update.jsx
@@ -1,25 +1,26 @@
 var SRUpdate = React.createClass({
     getInitialState: function() {
-        var component = this;
-        console.log('SRUpdate booted');
-        var item = { id: -1, service: '', description: '',  endpoint: '' };
+        var item = {id: -1, service: '', description: '', endpoint: ''};
         var qs = URI(location.href).query(true); // == e.g. { id : 4529987906437120 }
-        window.swagger = new SwaggerClient({
-            url: location.origin + "/swagger/swagger.json",
-            success: function() {
-                swagger.sr.id({id: qs.id},{responseContentType: 'application/json'}, function(data) {
-                    //document.getElementById("mydata").innerHTML = JSON.stringify(data.obj);
-                    //console.log(data.obj);
-                    component.setState({
-                        id: data.obj.id,
-                        service: data.obj.service,
-                        description: data.obj.description,
-                        endpoint: data.obj.endpoint
+        if(qs.id > 0) {
+            var component = this;
+            console.log('SRUpdate booted');
+            window.swagger = new SwaggerClient({
+                url: location.origin + "/swagger/swagger.json",
+                success: function () {
+                    swagger.sr.id({id: qs.id}, {responseContentType: 'application/json'}, function (data) {
+                        //document.getElementById("mydata").innerHTML = JSON.stringify(data.obj);
+                        //console.log(data.obj);
+                        component.setState({
+                            id: data.obj.id,
+                            service: data.obj.service,
+                            description: data.obj.description,
+                            endpoint: data.obj.endpoint
+                        });
                     });
-                });
-            }
-        });
-
+                }
+            });
+        }
         return {
             item,
             isSubmitting: true
@@ -37,18 +38,18 @@ var SRUpdate = React.createClass({
                                 </div>
                                 <div className="control-group">
                                     <label htmlFor="title" className="col-sm-2 control-label">Service:</label>
-                                    <input type="text" className="col-sm-10 form-control" id="title" onKeyPress={this.updateItem} required value={this.state.service} onChange={function(e){this.setState({service: e.target.value})}.bind(this)} defaultvalue placeholder="Enter any easy to remember service name." />
+                                    <input type="text" className="col-sm-10 form-control" id="title" onKeyDown={this.updateItem} required value={this.state.service} onChange={function(e){this.setState({service: e.target.value})}.bind(this)} defaultvalue placeholder="Enter any easy to remember service name." />
                                 </div>
                                 <div className="control-group">
                                     <label htmlFor="desc" className="col-sm-2 control-label">Description:</label>
                                     <div className="col-sm-10">
-                                        <input type="text" className="form-control" id="desc" onKeyPress={this.updateItem} value={this.state.description} onChange={function(e){this.setState({description: e.target.value})}.bind(this)} defaultvalue />
+                                        <input type="text" className="form-control" id="desc" onKeyDown={this.updateItem} value={this.state.description} onChange={function(e){this.setState({description: e.target.value})}.bind(this)} defaultvalue />
                                     </div>
                                 </div>
                                 <div className="control-group">
                                     <label htmlFor="endpoint" className="col-sm-2 control-label">URL:</label>
                                     <div className="col-sm-10">
-                                        <input type="url" className="form-control" id="endpoint" onKeyPress={this.updateItem} required value={this.state.endpoint} onChange={function(e){this.setState({endpoint: e.target.value})}.bind(this)} defaultvalue placeholder="Enter any valid URL e.g. http://www.google.com." />
+                                        <input type="url" className="form-control" id="endpoint" onKeyDown={this.updateItem} required value={this.state.endpoint} onChange={function(e){this.setState({endpoint: e.target.value})}.bind(this)} defaultvalue placeholder="Enter any valid URL e.g. http://www.google.com." />
                                     </div>
                                 </div>
                                 <div className="control-group">
@@ -68,26 +69,36 @@ var SRUpdate = React.createClass({
     goHome: function() {
         location.href='fusrstart.html';
     },
-    updateItem: function() {
-        var component = this;
-        window.swagger = new SwaggerClient({
-            url: location.origin + "/swagger/swagger.json",
-            success: function() {
-                var srJson = {
-                    sr: {
-                        id: component.state.id,
-                        service: component.state.service,
-                        description: component.state.description,
-                        endpoint: component.state.endpoint
-                    }
-                };
-                swagger.sr.save(srJson,{responseContentType: 'application/json'}, function(data) {
-                    //document.getElementById("mydata").innerHTML = JSON.stringify(data.obj);
-                    //console.log(data.obj);
-                });
-            }
-        });
-        console.log('SRUpdate input saved');
+    updateItem: function(e) {
+        var evt = e || window.event
+        // "e" is the standard behavior (FF, Chrome, Safari, Opera),
+        // while "window.event" (or "event") is IE's behavior
+        if ( evt.keyCode === 13 ) {
+            var component = this;
+            window.swagger = new SwaggerClient({
+                url: location.origin + "/swagger/swagger.json",
+                success: function() {
+                    var srJson = {
+                        sr: {
+                            id: component.state.id,
+                            service: component.state.service,
+                            description: component.state.description,
+                            endpoint: component.state.endpoint
+                        }
+                    };
+                    swagger.sr.save(srJson,{responseContentType: 'application/json'}, function(data) {
+                        //document.getElementById("mydata").innerHTML = JSON.stringify(data.obj);
+                        //console.log(data.obj);
+                        component.goHome();
+                    });
+                }
+            });
+            console.log('SRUpdate input saved');
+            // Do something
+
+            // You can disable the form submission this way:
+            return false
+        }
     },
     deleteItem: function() {
         var component = this;
@@ -97,6 +108,7 @@ var SRUpdate = React.createClass({
                 swagger.sr.delete({id: component.state.id},{responseContentType: 'application/json'}, function(data) {
                     //document.getElementById("mydata").innerHTML = JSON.stringify(data.obj);
                     //console.log(data.obj);
+                    component.goHome();
                 });
             }
         });
