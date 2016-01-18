a19dc52aed9163e28c7cc56d377a08ba89211e42
wf <wf@10.174.152.255>
2016-01-18 10:47:32 -0500

    SR2: updated Apimatic based on the latest Swagger specs and fixed some minor issues with AngularJS impl

--- war/n/js/apimatic/custom_config.js
+++ war/n/js/apimatic/custom_config.js
@@ -1,5 +1,5 @@
 <!-- override the APIMATIC settings with the current host/deployed env -->
-angular.module('ServiceManagerLib').factory('Configuration', function() {
+angular.module('CrudServiceLib').factory('Configuration', function() {
     return {
         BASEURI : location.origin
     };
