d0aafb854e6acbb2d5083bcd9b948a9e2e99e3ab
wf <white.flame72@gmail.com>
2016-02-06 09:12:11 -0500

    SR2: initial integration with ElasticSearch for reactjs search box

--- war/n/react/js/app-autocomplete.js
+++ war/n/react/js/app-autocomplete.js
@@ -45,6 +45,7 @@ var AppAutocomplete = React.createClass({
                     return _this.setState({ unitedStates: [] });
                 },
                 onChange: function (event, value) {
+//                    console.log('app-autocomplete.js onChange(): value [' + value + ']');
                     _this.setState({ loading: true });
                     fakeRequest(value, function (items) {
                         _this.setState({ unitedStates: items, loading: false });
@@ -86,7 +87,7 @@ var AppAutocomplete = React.createClass({
     },
 
     renderItems: function renderItems(items) {
-        console.log(items);
+        //console.log(items);
         return items.map(function (item, index) {
             var text = item.props.children;
             if (index === 0 || items[index - 1].props.children.charAt(0) !== text.charAt(0)) {
@@ -108,59 +109,85 @@ var AppAutocomplete = React.createClass({
     }
 });
 
-function getStates() {
-    return [
-        {abbr: "AL", name: "Alabama"},
-        {abbr: "AK", name: "Alaska"},
-        {abbr: "AZ", name: "Arizona"},
-        {abbr: "AR", name: "Arkansas"},
-        {abbr: "CA", name: "California"},
-        {abbr: "CO", name: "Colorado"},
-        {abbr: "CT", name: "Connecticut"},
-        {abbr: "DE", name: "Delaware"},
-        {abbr: "FL", name: "Florida"},
-        {abbr: "GA", name: "Georgia"},
-        {abbr: "HI", name: "Hawaii"},
-        {abbr: "ID", name: "Idaho"},
-        {abbr: "IL", name: "Illinois"},
-        {abbr: "IN", name: "Indiana"},
-        {abbr: "IA", name: "Iowa"},
-        {abbr: "KS", name: "Kansas"},
-        {abbr: "KY", name: "Kentucky"},
-        {abbr: "LA", name: "Louisiana"},
-        {abbr: "ME", name: "Maine"},
-        {abbr: "MD", name: "Maryland"},
-        {abbr: "MA", name: "Massachusetts"},
-        {abbr: "MI", name: "Michigan"},
-        {abbr: "MN", name: "Minnesota"},
-        {abbr: "MS", name: "Mississippi"},
-        {abbr: "MO", name: "Missouri"},
-        {abbr: "MT", name: "Montana"},
-        {abbr: "NE", name: "Nebraska"},
-        {abbr: "NV", name: "Nevada"},
-        {abbr: "NH", name: "New Hampshire"},
-        {abbr: "NJ", name: "New Jersey"},
-        {abbr: "NM", name: "New Mexico"},
-        {abbr: "NY", name: "New York"},
-        {abbr: "NC", name: "North Carolina"},
-        {abbr: "ND", name: "North Dakota"},
-        {abbr: "OH", name: "Ohio"},
-        {abbr: "OK", name: "Oklahoma"},
-        {abbr: "OR", name: "Oregon"},
-        {abbr: "PA", name: "Pennsylvania"},
-        {abbr: "RI", name: "Rhode Island"},
-        {abbr: "SC", name: "South Carolina"},
-        {abbr: "SD", name: "South Dakota"},
-        {abbr: "TN", name: "Tennessee"},
-        {abbr: "TX", name: "Texas"},
-        {abbr: "UT", name: "Utah"},
-        {abbr: "VT", name: "Vermont"},
-        {abbr: "VA", name: "Virginia"},
-        {abbr: "WA", name: "Washington"},
-        {abbr: "WV", name: "West Virginia"},
-        {abbr: "WI", name: "Wisconsin"},
-        {abbr: "WY", name: "Wyoming"}
-    ];
+var searchedResults = [];
+var arr = [];
+var r = {};
+function getES(term) {
+    $.get('https://es-n3t.rhcloud.com/service_registry/_search?size=100&pretty&q='+term, function(result) {
+        if(typeof result !== 'undefined') {
+            //console.log('app-autocomplete.js getES(): term [' + term + '] result [' + result.hits.total + ']');
+            r = result.hits.hits;
+            //console.log(r);
+            var h;
+            for(var i = 0; i<r.length; i++) {
+                h = r[i];
+                arr.push(h._source);
+            }
+            searchedResults = arr.reduce(function(all, item, index) {
+                all.push({abbr: item.service, name: item.summary});
+                return all;
+            }, []);
+        }
+    });
+}
+
+function getStates(value) {
+    getES(value);
+
+    //TODO to use JavaScript reduce to convert ES results into the following format! :p
+
+    return searchedResults;     //[
+//        {abbr: "AL", name: "Alabama"},
+//        {abbr: "AK", name: "Alaska"},
+//        {abbr: "AZ", name: "Arizona"},
+//        {abbr: "AR", name: "Arkansas"},
+//        {abbr: "CA", name: "California"},
+//        {abbr: "CO", name: "Colorado"},
+//        {abbr: "CT", name: "Connecticut"},
+//        {abbr: "DE", name: "Delaware"},
+//        {abbr: "FL", name: "Florida"},
+//        {abbr: "GA", name: "Georgia"},
+//        {abbr: "HI", name: "Hawaii"},
+//        {abbr: "ID", name: "Idaho"},
+//        {abbr: "IL", name: "Illinois"},
+//        {abbr: "IN", name: "Indiana"},
+//        {abbr: "IA", name: "Iowa"},
+//        {abbr: "KS", name: "Kansas"},
+//        {abbr: "KY", name: "Kentucky"},
+//        {abbr: "LA", name: "Louisiana"},
+//        {abbr: "ME", name: "Maine"},
+//        {abbr: "MD", name: "Maryland"},
+//        {abbr: "MA", name: "Massachusetts"},
+//        {abbr: "MI", name: "Michigan"},
+//        {abbr: "MN", name: "Minnesota"},
+//        {abbr: "MS", name: "Mississippi"},
+//        {abbr: "MO", name: "Missouri"},
+//        {abbr: "MT", name: "Montana"},
+//        {abbr: "NE", name: "Nebraska"},
+//        {abbr: "NV", name: "Nevada"},
+//        {abbr: "NH", name: "New Hampshire"},
+//        {abbr: "NJ", name: "New Jersey"},
+//        {abbr: "NM", name: "New Mexico"},
+//        {abbr: "NY", name: "New York"},
+//        {abbr: "NC", name: "North Carolina"},
+//        {abbr: "ND", name: "North Dakota"},
+//        {abbr: "OH", name: "Ohio"},
+//        {abbr: "OK", name: "Oklahoma"},
+//        {abbr: "OR", name: "Oregon"},
+//        {abbr: "PA", name: "Pennsylvania"},
+//        {abbr: "RI", name: "Rhode Island"},
+//        {abbr: "SC", name: "South Carolina"},
+//        {abbr: "SD", name: "South Dakota"},
+//        {abbr: "TN", name: "Tennessee"},
+//        {abbr: "TX", name: "Texas"},
+//        {abbr: "UT", name: "Utah"},
+//        {abbr: "VT", name: "Vermont"},
+//        {abbr: "VA", name: "Virginia"},
+//        {abbr: "WA", name: "Washington"},
+//        {abbr: "WV", name: "West Virginia"},
+//        {abbr: "WI", name: "Wisconsin"},
+//        {abbr: "WY", name: "Wyoming"}
+//    ];
 }
 
 function matchStateToTerm(state, value) {
@@ -178,8 +205,8 @@ function sortStates(a, b, value) {
 }
 
 function fakeRequest(value, cb) {
-    if (value === '') return getStates();
-    var items = getStates().filter(function (state) {
+    if (value === '') return getStates(value);
+    var items = getStates(value).filter(function (state) {
         return matchStateToTerm(state, value);
     });
     setTimeout(function () {
