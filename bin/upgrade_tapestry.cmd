56f5d200e6ebd5fc47e79f7edcfc8cd4fcf0d080
wf <white.flame72@gmail.com>
2016-03-05 13:33:12 -0500

    Updated jar files with production release of 5.4

--- bin/upgrade_tapestry.cmd
+++ bin/upgrade_tapestry.cmd
@@ -1,16 +1,18 @@
-set T_VERSION=5.3.8
+set T_VERSION=5.4.0
+
+set PROJECT_HOME=c:\Users\A\GoogleAppEngine
 
 REM goto http://tapestry.apache.org/download.html, download all jar files and copy them into 1/
 
-cd %USERPROFILE%\Downloads\apache-tapestry-5.3.8-bin
+cd %USERPROFILE%\Downloads\apache-tapestry-%T_VERSION%-bin
 
-copy tapestry-core-%T_VERSION%.jar c:\GoogleAppEngine\war\WEB-INF\lib
-copy tapestry-func-%T_VERSION%.jar c:\GoogleAppEngine\war\WEB-INF\lib
-copy tapestry-ioc-%T_VERSION%.jar c:\GoogleAppEngine\war\WEB-INF\lib
-copy tapestry-json-%T_VERSION%.jar c:\GoogleAppEngine\war\WEB-INF\lib
-copy tapestry-test-%T_VERSION%.jar c:\GoogleAppEngine\war\WEB-INF\lib
-copy tapestry-yuicompressor-%T_VERSION%.jar c:\GoogleAppEngine\war\WEB-INF\lib
-copy tapestry5-annotations-%T_VERSION%.jar c:\GoogleAppEngine\war\WEB-INF\lib
-copy plastic-%T_VERSION%.jar c:\GoogleAppEngine\war\WEB-INF\lib
+copy tapestry-core-%T_VERSION%.jar %PROJECT_HOME%\war\WEB-INF\lib
+copy tapestry-func-%T_VERSION%.jar %PROJECT_HOME%\war\WEB-INF\lib
+copy tapestry-ioc-%T_VERSION%.jar %PROJECT_HOME%\war\WEB-INF\lib
+copy tapestry-json-%T_VERSION%.jar %PROJECT_HOME%\war\WEB-INF\lib
+copy tapestry-test-%T_VERSION%.jar %PROJECT_HOME%\war\WEB-INF\lib
+rem copy tapestry-yuicompressor-%T_VERSION%.jar %PROJECT_HOME%\war\WEB-INF\lib
+copy tapestry5-annotations-%T_VERSION%.jar %PROJECT_HOME%\war\WEB-INF\lib
+copy plastic-%T_VERSION%.jar %PROJECT_HOME%\war\WEB-INF\lib
 
 pause	
