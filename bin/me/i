b55f0874c55faeb7a88e3bc73cf063514db0b8e1
wf <white.flame72@gmail.com>
2016-03-19 10:49:38 -0400

    2Share: fixed Parse issue

---
+++ bin/me/i
@@ -0,0 +1,1 @@
+npm i && npm nodemon -g
--- war/parse/facebook.js
+++ war/parse/facebook.js
@@ -5,68 +5,6 @@ var gMovieURL = '/mcrud/movie.html';
 //gLogoutURL = '/parse/index1.html';
 var gLogoutURL = App.login_url;
 
-//$(document).ready(function () {
-////TBD not able to put this in AMD and make it to work, thus it is here! :(
-//Parse.initialize("Ld70ODLxjXFkhhRux6kQqVCiJ4rQeXU8dISafNJa", "dLHhDNKnLimXHzSzxvQcGuwle5iLwnn3bFahDS9q");
-////window.console && console.log("main-config: Parse initialized (facebook.js).");
-////=== https://www.parse.com/tutorials/session-migration-tutorial
-//Parse.User.enableRevocableSession();
-//});
-
-(function(d, debug){
-    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement('script'); js.id = id; js.async = true;
-    js.src = "//connect.facebook.net/en_US/all" + (debug ? "/debug" : "") + ".js";
-    ref.parentNode.insertBefore(js, ref);
-}(document, false));
-
-window.fbAsyncInit = function() {
-    //initialize facebook SDK via parse
-    var realAppId;
-    if(location.hostname.indexOf('cbiit') > -1 || location.hostname.indexOf('cadsr') > -1) {
-        realAppId = '1391742604391122';
-    }
-    else
-    if(location.hostname.indexOf('teev.rhc') > -1) {
-        realAppId = '395343103935527';
-    }
-    else
-    if(location.hostname.indexOf('oo.tv') > -1 || location.hostname.indexOf('hudoone') > -1) {
-        realAppId = '522052734496612';
-    }
-    else
-    if(location.hostname.indexOf('service') > -1) {
-        realAppId = '333890893403449';
-    }
-    else
-    if(location.hostname.indexOf('share') > -1) {
-        realAppId = '351597214975046';
-    }
-    else
-    if(location.hostname.indexOf('aware') > -1) {
-        realAppId = '1434646580086725';
-    }
-    else
-    if(location.hostname.indexOf('localhost') > -1) {
-        realAppId = '149411878572809';
-    }
-    else {
-    	window.console && console.log("ERROR: No appdomain registered with Facebook, please visit https://developers.facebook.com/apps to create one!!!");
-        realAppId = '149411878572809';
-    }
-
-    Parse.FacebookUtils.init({
-        status: false,  //https://parse.com/questions/what-does-a-non-null-parseusercurrent-guarantee
-        'appId'      : realAppId,
-        'channelUrl' : '/channel.html',
-        'cookie'     : true,
-        'xfbml'      : true
-    });
-//  status     : true, // check the login status upon init?
-    //Code to be executed after initializing the Facebook SDK.
-};
-
 function logout() {
     var r = confirm("Are you sure? This will log you out from the application.");
     if (r === false)
@@ -132,36 +70,16 @@ function doLogout() {
 
     $("#gp-login-notice").hide();
 
-    //try {
-    //    //window.console && console.log("removing app client session ...");
-    //    var removeSessionUrl = '/jsp/logout2Share.jsp';
-    //    $.ajax({
-    //        type: 'GET',
-    //        url: removeSessionUrl,
-    //        async: false,
-    //        success: function(data) {
-    //            //purgeStates();
-    //            window.location=gLogoutURL;
-    //        },
-    //        error: function(e) {
-    //            window.console && console.log("session cleanup error: " + e.message + "]");
-    //            alert("Logout error: " + e.message + ". Please try again.");
-    //
-    //        }
-    //    });
-    //} catch(e) {
-    //    window.console && console.log("Not able to logout and clean up the server side session, error: " + e);
-    //}
     //JWT to clear the token!
     localStorage.removeItem('2shareJWTToken');
 
     window.location=gLogoutURL;
-
-    //window.location=gLogoutURL;
 }
 
 function logoutParse() {
-    if(Parse.User.current() !== undefined) {
+    //debugger
+
+    if(typeof Parse !== 'undefined' && typeof Parse.User.current() !== 'undefined') {
         Parse.User.logOut();
 //        try {
 //            FB.logout();    //as we are using the native Facebook <fb:login-button ...
@@ -170,10 +88,3 @@ function logoutParse() {
 //        }
     }
 }
-
-//define('facebook', [], function() {
-//    //does nothing for now - just to suppress "Uncaught Error: No define call for ..." error
-//    return function() {
-//    };
-//});
-
