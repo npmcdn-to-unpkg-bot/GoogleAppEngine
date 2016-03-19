d6640e44abd342b0fe6d10835403b326fc797efd
white.flame <white.flame72@gmail.com>
2016-03-18 23:55:50 -0400

    Update Windows shortcuts

--- bin/setEnv.cmd
+++ bin/setEnv.cmd
@@ -2,14 +2,14 @@
 :cd ..\..
 set PROJECT_HOME=..
 
-set JAVA_HOME=C:\jdk1.7.0_80
+set JAVA_HOME=\jdk1.7.0_80
 rem 
-set GAE_JAVA_SDK_HOME=C:\appengine-java-sdk-1.9.12
+set GAE_JAVA_SDK_HOME=\appengine-java-sdk-1.9.12
 
-set GAE_JAVA_SDK_HOME=C:\appengine-java-sdk-1.9.32
-set MAVEN_HOME=C:\apache-maven-2.2.1
-set ROO_HOME=C:\spring-roo-1.1.0.M3
-set ANT_HOME=C:\apache-ant-1.6.5
+set GAE_JAVA_SDK_HOME=\appengine-java-sdk-1.9.32
+set MAVEN_HOME=\apache-maven-2.2.1
+set ROO_HOME=\spring-roo-1.1.0.M3
+set ANT_HOME=\apache-ant-1.6.5
 
 set PATH=%JAVA_HOME%\bin;%GAE_JAVA_SDK_HOME%\bin;%MAVEN_HOME%\bin;%ROO_HOME%\bin;%ANT_HOME%\bin;%PATH%
 
