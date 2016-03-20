bb06a68d08720a7952f44e5957c61833d94e66d7
white.flame <white.flame72@gmail.com>
2016-03-20 12:27:33 -0400

    Fix r on windows

--- bin/r.bat
+++ bin/r.bat
@@ -1,3 +1,3 @@
-call setEnv.cmd
+call setenv.bat
 
 java -Dtapestry.execution-mode=DevelopmentMode -javaagent:%GAE_JAVA_SDK_HOME%/lib/agent/appengine-agent.jar -classpath %GAE_JAVA_SDK_HOME%/lib/appengine-tools-api.jar com.google.appengine.tools.development.DevAppServerMain --port=8888 --address=0.0.0.0 %PROJECT_HOME%/war
