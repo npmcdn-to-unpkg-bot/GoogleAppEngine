1344f6fe620df7fbaf5432fe68fdc046d8b21edb
adcoolguy <androidcoolguy@hotmail.com>
2016-06-14 13:51:43 -0400

    SR2: fixed some issues with angularjs example

---
+++ war/n/ng/app-ng.js
@@ -0,0 +1,183 @@
+'use strict';
+// var angular = require('angular');
+// require('angular-ui-router');
+
+var gVersion = "0.0.1R6";
+
+function redirectNonSSL(url) {
+    if(typeof url !== 'undefined'  && url.trim().indexOf('localhost') == -1 && url.trim().startsWith('http://')) {
+        if (window.location.protocol != "https:")
+            window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
+    }
+}
+function handleSSL(url) {
+    if(typeof url !== 'undefined'  && url.trim().indexOf('localhost') == -1 && url.trim().startsWith('http://')) {
+        var ret = url.trim().replace('http://', 'https://');
+        console.log('url changed to SSL-based -> [' + ret + ']');
+        return ret;
+    } else {
+        return url;
+    }
+}
+
+redirectNonSSL(window.location.origin);
+console.log('redirectNonSSL invoked');
+
+//console.log("app.js 2 $state:");
+//console.log($state);
+
+//=== JWT and navigations stuff
+angular.module('myApp', ['ui.router'])
+    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/home');
+
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: '../ng/fusrstart.html',
+                controller: function($scope, $stateParams, $state) {
+                    window.$state = $state; //TODO how do we avoid global here???!!!
+                    if(typeof SRStart !== 'undefined') {
+                        ReactDOM.render(React.createElement(SRStart, {$state: $state}), document.getElementById('sr-start'));
+                    }
+                }
+            })
+            .state('update', {
+                url: '/update/{id:int}',
+                templateUrl: '../ng/fusrupdate.html',
+                controller: function($scope, $stateParams, $state) {
+                    //$scope.inboxId = $stateParams.inboxId;
+                    //console.log('app.js $stateProvider update: ', $stateParams);
+                    var id = $stateParams.id;    //$stateParams.obj.id;
+                    //console.log('app.js id = [' + id + ']');
+                    ReactDOM.render(React.createElement(SRUpdate, {$state: $state, id: id}), document.getElementById('sr-update'));
+                },
+                params: {
+                    obj: null
+                }
+            })
+            .state('create', {
+                url: '/create',
+                templateUrl: '../ng/fusrcreate.html',
+                controller: function($scope, $stateParams, $state) {
+                    //$scope.inboxId = $stateParams.inboxId;
+                    ReactDOM.render(React.createElement(SRCreate, {$state: $state}), document.getElementById('sr-create'));
+                }
+            });
+
+
+    }])
+    .controller('MainCtrl', ['mainService','$scope','$http', '$state',
+        function(mainService, $scope, $http, $compile, $state) {
+            $scope.version = gVersion;
+            $scope.status = 'Please sign in';
+            $scope.greeting = 'Welcome!';
+            $scope.token = null;
+            $scope.error = null;
+            $scope.roleUser = false;
+            $scope.roleAdmin = false;
+            $scope.roleFoo = false;
+
+            $scope.login = function() {
+                $scope.status = "Logging in ...";
+                $scope.error = null;
+                mainService.login($scope.userName, $scope.passWord).then(function(token) {
+                    $scope.token = token;
+                    $http.defaults.headers.common.Authorization = 'Bearer ' + token;
+                    $scope.checkRoles();
+                },
+                function(error){
+                    $scope.status = "Invalid login. Please try again.";
+                    $scope.error = error
+                    $scope.userName = '';
+                    $scope.passWord = '';
+                });
+            }
+
+            $scope.checkRoles = function() {
+                mainService.hasRole('user').then(function(user) {$scope.roleUser = user});
+                mainService.hasRole('admin').then(function(admin) {$scope.roleAdmin = admin});
+                mainService.hasRole('foo').then(function(foo) {$scope.roleFoo = foo});
+            }
+
+            $scope.logout = function() {
+                $scope.userName = '';
+                $scope.token = null;
+                $http.defaults.headers.common.Authorization = '';
+                location.href = "index.html";
+                // Clear the JWT token.
+                localStorage.removeItem('userJWTToken');
+            }
+
+            $scope.loggedIn = function() {
+                var token = $scope.token;
+                if(token !== null) {
+                    // Save the JWT token.
+                    localStorage.setItem('userJWTToken', token);
+                    location.href = "fusrstart.html";
+                }
+                return token !== null;
+            }
+
+            $scope.enter = function() {
+                location.href='fusrstart.html';
+            }
+
+        } ])
+    .service('mainService', ['$http', function($http) {
+        return {
+            login : function(username, pwd) {
+                return $http.post('/api/user/login', {name: username, password: pwd}).then(function(response) {
+                    return response.data.token;
+                });
+            },
+
+            hasRole : function(role) {
+                return $http.get('/api/jwt/role/' + role).then(function(response){
+                    console.log(response);
+                    return response.data;
+                });
+            }
+        };
+    }]);
+
+// Common directive for Focus
+angular.module('myApp').directive('focus', ['$timeout',
+    function ($timeout) {
+        return {
+            scope: {
+                trigger: '@focus'
+            },
+            link: function (scope, element) {
+                scope.$watch('trigger', function (value) {
+                    if (value === "true") {
+                        $timeout(function () {
+                            element[0].focus();
+                        });
+                    }
+                });
+            }
+        };
+    }]
+);
+
+// window.onerror = function(msg, url, line, col, error) {
+//     // Note that col & error are new to the HTML 5 spec and may not be
+//     // supported in every browser.  It worked for me in Chrome.
+//     var extra = !col ? '' : '\ncolumn: ' + col;
+//     extra += !error ? '' : '\nerror: ' + error;
+
+//     // You can view the information in an alert to see things working like this:
+//     // alert("n/ng/app.js onerror: " + msg + "\nurl: " + url + "\nline: " + line + extra);
+//     console.error("n/ng/app.js onerror: " + msg + "\nurl: " + url + "\nline: " + line + extra);
+
+//     // TODO: Report this error via ajax so you can keep track
+//     //       of what pages have JS issues
+
+//     var suppressErrorAlert = true;
+//     // If you return true, then error alerts (like in older versions of
+//     // Internet Explorer) will be suppressed.
+//     return suppressErrorAlert;
+// };
+
+// angular.bootstrap(document, ['myApp']);
---
+++ war/readme.txt
@@ -0,0 +1,14 @@
+You need to build this directory to make sure the application works correctly.
+
+You also need to make sure that you have nodist installed correctly.
+
+Start the build
+---------------
+
+cd war
+
+i
+
+b
+
+Enjoy!
