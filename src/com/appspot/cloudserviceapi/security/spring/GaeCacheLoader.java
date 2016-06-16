da4d36a8b4b2540ac54a70be48c1a9e84fd95fa8
adcoolguy <androidcoolguy@hotmail.com>
2016-06-16 13:09:12 -0400

    More work related to user login attempts counts

---
+++ src/com/appspot/cloudserviceapi/security/spring/GaeCacheLoader.java
@@ -0,0 +1,16 @@
+package com.appspot.cloudserviceapi.security.spring;
+
+import com.google.common.cache.CacheLoader;
+
+public class GaeCacheLoader extends CacheLoader {
+//	@Override
+//	public Animal load(String key) throws Exception {
+//		return new Animal(key);
+//	}
+
+	@Override
+	public Object load(Object arg0) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
